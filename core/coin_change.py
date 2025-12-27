c5e3b11fe73196165870ba16964c2b881087778a
cecegoretti <cece.goretti@gmail.com>
2025-12-27 12:10:33 +0100

    Update coin_change.py

--- core/coin_change.py
+++ core/coin_change.py
@@ -1,13 +1,20 @@
 from collections import deque
 
-def coin_change(coins_value, amount):
+def coin_change(coin_values, amount):
   ''' this function computes the minimum number of coins 
       needed to obtain the requested amount. We assume that we
       can use as many coins of each type as we wish.
       The idea is to use a queue to
       simulate an analogous of a BFS tree search on 
       the amounts that can be obtained from the set 
-      of coins'''
+      of coins.
+      Space complexity: O(amount)
+      Time complexity: O(amount*len(coin_values).
+      Note: if gcd of coin_values is >1, than BFS approach
+      is optimal compared to classical DP. If gcd=1 and
+      amount big enough, the two approach are computationally
+      equivalent.
+      '''
   if amount<0:
     return -1
   if amount==0:
@@ -18,7 +25,7 @@ def coin_change(coins_value, amount):
   new_found=deque([0])
   while new_found:
     last_found=new_found.popleft()
-    for i in coins_value:
+    for i in coin_values:
       if last_found+i<=amount:
         if dp[last_found+i]==0:
           #last_found+i>0 hence we are checking 
